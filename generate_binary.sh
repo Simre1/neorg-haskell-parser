c58ce26b674ac959ed818932725c933c1e85f0dd
Simre1 <simre4775@gmail.com>
2023-03-12 03:25:20 +0100

    Remove old files

--- generate_binary.sh
+++
@@ -1,4 +0,0 @@
-cabal build -O2
-cp dist-newstyle/build/x86_64-linux/ghc-8.10.7/neorg-0.1.0.0/x/neorg-pandoc/build/neorg-pandoc/neorg-pandoc release/neorg-pandoc-linux86 &&
-strip release/neorg-pandoc-linux86 &&
-upx release/neorg-pandoc-linux86 
